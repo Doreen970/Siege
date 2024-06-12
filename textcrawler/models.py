d89e2e982a015a22905757c29492125c14a92da2
Doreen970 <doreenwanyama20@gmail.com>
2024-06-13 00:19:00 +0300

    I created my model that will be used to store my data. Since my model data will have timestamp eg there will be checking of time, I modified the timezone in settings.py to match my timezone.

--- textcrawler/models.py
+++ textcrawler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# A model that will log the status of the crawling processs
+class CrawlLog(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=20)
+    error_message = models.TextField(null=True, blank=True)
